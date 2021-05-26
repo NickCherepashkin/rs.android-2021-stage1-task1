18ad438b5704fb56981dd881de2d05857e8fcc36
Nick Cherepashkin <cherepawka13@mail.ru>
2021-05-26 18:47:04 +0300

    subtask_1

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,31 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val array = IntArray(sadArray.size)
+        var countElem = 0
+        try {
+            for (i in 0 until sadArray.size ) {
+                if ((i > 0) && (i < (sadArray.size - 1))) {
+                    if (!(sadArray[i] > (sadArray[i-1] + sadArray[i + 1]))) {
+                        array[countElem] = sadArray[i]
+                        countElem++
+                    }
+                } else if (i == 0 || i == sadArray.size - 1) {
+                    array[countElem] = sadArray[i]
+                    countElem++
+                }
+            }
+            if(countElem == sadArray.size) {
+                return array
+            } else {
+                val resultArray = array.dropLast(sadArray.size - countElem)
+                val res = IntArray(resultArray.size)
+                for (i in 0 until resultArray.size) res[i] = resultArray[i]
+
+                return convertToHappy(res)
+            }
+        } catch (e: NotImplementedError) {
+            throw NotImplementedError("Not implemented")
+        }
     }
 }
